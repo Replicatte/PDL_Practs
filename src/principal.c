f3c205a987da35dfc364e4799e0ff74db61b9522
ALMENDROS GRANERO <joralgra@EVIRL-021-OK.dsic.cloud>
2019-01-12 21:57:59 +0100

    VolcarCodigo en principal.c

--- src/principal.c
+++ src/principal.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "header.h"
+#include "libgci.h"
 
 int verTDS=FALSE;                   /* Flag para saber si mostrar la TDS     */
 int verbosidad=FALSE;               /* Flag para saber si se desea una traza */
@@ -18,10 +19,10 @@ void yyerror(const char * msg)
 /*****************************************************************************/
 int main (int argc, char **argv) 
 /* Gestiona la linea de comandos e invoca al analizador sintactico-semantico.*/
-{ int i, n = 1;
+{ int i, n = 1; char *nom_fich;
 
   for (i=1; i<argc; ++i) { 
-    if      (strcmp(argv[i], "-v")==0) { verbosidad = TRUE; n++; }
+    if (strcmp(argv[i], "-v")==0) { verbosidad = TRUE; n++; }
     else if (strcmp(argv[i], "-t")==0) { verTDS = TRUE; n++; }
   }
   if (argc == n+1) {
@@ -31,13 +32,14 @@ int main (int argc, char **argv)
     }
     else {        
       if (verbosidad == TRUE) fprintf(stdout,"%3d.- ", yylineno);
+      nom_fich = argv[n];
       yyparse ();
-      if (numErrores > 0) 
-        fprintf(stderr,"\nNumero de errores:      %d\n", numErrores);
+      if (numErrores == 0) volcarCodigo(nom_fich);
+      else fprintf(stderr,"\nNumero de errores:      %d\n", numErrores);
     }   
   }
   else fprintf (stderr, "Uso: cmc [-v] [-t] fichero\n");
-  
-  return (0);
+
+  return(0);
 } 
 /*****************************************************************************/
